b649a1b71826422f9367fd76783222f4d323168c
Miranda <mirjade2008@yahoo.com>
2022-03-08 19:49:21 -0500

    please work

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-export const BACKEND_URL = process.env.NODE_ENV === "development"  ? "http://localhost:5000/"  : "https://ryan-caps-alot.herokuapp.com/";
+export const BACKEND_URL = process.env.NODE_ENV === "development"  ? "http://localhost:5000/"  : "https://miranda-capstone.herokuapp.com/";
